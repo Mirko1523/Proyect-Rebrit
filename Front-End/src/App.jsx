1ce03f608560ac38f3737482198bc482820bedf2
Mirko1523 <134454433+Mirko1523@users.noreply.github.com>
2024-07-23 15:51:50 -0300

    first commit

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import './css/style.css';
+import './charts/ChartjsConfig';
+import Dashboard from './pages/Dashboard';
+import Signin from './components/components2/SignIn';
+
+function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); 
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+        <Route exact path="/login" element={<Signin />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ Front-End/src/components/components2/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Signin (){
+    
+return(
+    <div>
+<p>proximamente un login</p>
+    </div>
+)
+}
+
+export default Signin;
